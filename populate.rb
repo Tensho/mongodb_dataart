c5c5895a28daf203bf8c93a45ba64ffcd4474c12
Andrew Babichev <ababichev@dataart.com>
2014-08-20 18:48:06 +0300

    Init populate

--- populate.rb
+++ populate.rb
@@ -4,3 +4,24 @@ Bundler.require
 ENV['MONGOID_ENV'] = 'development'
 
 Mongoid.load!("config/mongoid.yml")
+
+require_relative 'initializers/faker_position'
+require_relative 'user'
+
+10.times do
+  phone = Phone.new work: Faker::PhoneNumber.phone_number,
+                    home: Faker::PhoneNumber.phone_number,
+                    mobile: Faker::PhoneNumber.phone_number
+
+  User.create full_name: Faker::Name.name,
+              birthday: Faker::Date.birthday,
+              email: Faker::Internet.email,
+              phone: phone,
+              position: Faker::Name.position,
+              status: [true, false].sample,
+              # photo: Moped::BSON::Binary.new,
+              domain_login: Faker::Internet.domain_name,
+              wish_users: []
+end
+
+
