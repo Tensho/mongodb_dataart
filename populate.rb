7387ad9e4d3a092b11c07ce35dafdb35af7707c9
Andrew Babichev <ababichev@dataart.com>
2014-08-21 13:34:26 +0300

    Populate projects

--- populate.rb
+++ populate.rb
@@ -9,6 +9,8 @@ require_relative 'initializers/faker_position'
 require_relative 'user'
 require_relative 'project'
 
+# TODO: clean db before population
+
 10.times do
   phone = Phone.new work: Faker::PhoneNumber.phone_number,
                     home: Faker::PhoneNumber.phone_number,
@@ -22,7 +24,21 @@ require_relative 'project'
               status: [true, false].sample,
               # photo: Moped::BSON::Binary.new,
               domain_login: Faker::Internet.domain_name,
-              wish_users: []
+              wish_users: User.all.sample(rand(10))
 end
+
+10.times do
+  start_date = Faker::Date.backward
+  end_date = start_date + rand(365)
+
+  Project.create name: Faker::Company.name,
+                 start_date: start_date,
+                 end_date: end_date,
+                 status: ['opened', 'closed', 'deferred'].sample,
+                 budget: Faker::Number.between(500, 10000),
+                 manager: User.all.sample,
+                 participants: User.all.sample(rand(10) + 1)
+end
+
 
 
