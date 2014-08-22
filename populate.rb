baac4fbdafd29af0e3c2a5d702dc05a723cc130a
Andrew Babichev <ababichev@dataart.com>
2014-08-22 10:55:42 +0300

    Batch insert

--- populate.rb
+++ populate.rb
@@ -12,61 +12,79 @@ require_relative 'task'
 require_relative 'duration'
 
 Mongoid.purge!
-# TODO: bulk insert
 
-10.times do
+BATCH_SIZE = 1000
+
+def batch_insert(model, n)
+  n.times do
+    collection = []
+
+    BATCH_SIZE.times do
+      collection << yield
+    end
+
+    model.create(collection)
+  end
+end
+
+batch_insert(User, 1) do
   phone = Phone.new work: Faker::PhoneNumber.phone_number,
                     home: Faker::PhoneNumber.phone_number,
                     mobile: Faker::PhoneNumber.phone_number
 
-  User.create full_name: Faker::Name.name,
-              birthday: Faker::Date.birthday,
-              email: Faker::Internet.email,
-              phone: phone,
-              position: Faker::Name.position,
-              status: [true, false].sample,
-              # photo: Moped::BSON::Binary.new,
-              domain_login: Faker::Internet.domain_name,
-              wish_users: User.all.sample(rand(10))
+  {
+    full_name: Faker::Name.name,
+    birthday: Faker::Date.birthday,
+    email: Faker::Internet.email,
+    phone: phone,
+    position: Faker::Name.position,
+    status: [true, false].sample,
+    # photo: BSON::Binary.new,
+    domain_login: Faker::Internet.domain_name,
+    wish_users: User.all.sample(rand(10))
+  }
 end
 
-10.times do
+batch_insert(Project, 1) do
   start_date = Faker::Date.backward
   end_date = start_date + rand(365)
 
-  Project.create name: Faker::Company.name,
-                 start_date: start_date,
-                 end_date: end_date,
-                 status: ['opened', 'closed', 'deferred'].sample,
-                 budget: Faker::Number.between(500, 10000),
-                 manager: User.all.sample,
-                 participants: User.all.sample(rand(10) + 1)
+  {
+    name: Faker::Company.name,
+    start_date: start_date,
+    end_date: end_date,
+    status: ['opened', 'closed', 'deferred'].sample,
+    budget: Faker::Number.between(500, 10000),
+    manager: User.all.sample,
+    participants: User.all.sample(rand(10) + 1)
+  }
 end
 
-10.times do
+batch_insert(Task, 1) do
   start_date = Faker::Date.backward
   end_date = start_date + rand(365)
 
-  Task.create name: Faker::Hacker.say_something_smart,
-              start_date: start_date,
-              end_date: end_date,
-              status: ['opened', 'in_progress', 'done', 'reopened', 'closed'].sample,
-              description: Faker::Lorem.sentence,
-              project: Project.all.sample,
-              responsible: User.all.sample
+  {
+    name: Faker::Hacker.say_something_smart,
+    start_date: start_date,
+    end_date: end_date,
+    status: ['opened', 'in_progress', 'done', 'reopened', 'closed'].sample,
+    description: Faker::Lorem.sentence,
+    project: Project.all.sample,
+    responsible: User.all.sample
+  }
 end
 
-10.times do
+batch_insert(Duration, 1) do
   date = Faker::Date.backward
   time = date.to_time + rand(60 * 60 * 60)
 
-  Duration.create description: Faker::Lorem.paragraph,
-                  date: date,
-                  time: time,
-                  user: User.all.sample,
-                  project: Project.all.sample,
-                  task: Task.all.sample
+  {
+    description: Faker::Lorem.paragraph,
+    date: date,
+    time: time,
+    user: User.all.sample,
+    project: Project.all.sample,
+    task: Task.all.sample
+  }
 end
-
-
-
